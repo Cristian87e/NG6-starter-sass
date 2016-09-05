440c51996f9042add899ecb4332d70977a9907cf
Cristian <cristianperrone.dev@gmail.com>
2016-09-04 21:58:07 -0300

    Fix some Styles

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,9 +1,11 @@
 import angular from 'angular';
+import Header from './header/header';
 import Navbar from './navbar/navbar';
 import User from './user/user';
 
 let commonModule = angular.module('app.common', [
   Navbar.name,
+  Header.name,
   User.name
 ]);
 
